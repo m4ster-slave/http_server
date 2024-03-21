165c843ad0591656e797e391ba69fa4fa51f50cc
rpgyumyum <CarterMoony@proton.me>
2024-03-21 15:32:51 +0100

    second stage

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use std::net::TcpListener;
+use std::{
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() 
 {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() 
     {
-        match stream 
-        {
-            Ok(_stream) => 
+            match stream 
             {
-                 println!("accepted new connection");
+                Ok (_stream) => 
+                {
+                    println!("accepted new connection");
+                    handle_conn(_stream);
+                }
+                Err (e) => 
+                {
+                    println!("error: {}", e);
+                }
             }
-            Err(e) => 
-            {
-                 println!("error: {}", e);
-            }
-         }
+
+
      }
+}
+
+fn handle_conn (mut stream: TcpStream)
+{
+    let response = "HTTP/1.1 200 OK";
+    stream.write_all(response.as_bytes()).unwrap();
 }
